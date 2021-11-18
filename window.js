64dda374dd8971afa8e32570fb975ee3613f8e49
Taufiqul Islam <48012661+Taufiqul7756@users.noreply.github.com>
2021-11-18 17:43:37 +0600

    global variable check

--- window.js
+++ window.js
@@ -3,8 +3,14 @@ var name = "Kuddus"; //Global variable . we can use both inside and outside
 function add(num1, num2) {
   var result = num1 + num2;
   console.log("Name inside:", name);
-  return result;
+  function double(num) {
+    return num * 2;
+  }
+  var total = double(result);
+  console.log(total);
 }
 var sum = add(10, 12);
 console.log(sum);
 console.log("Name outside:", name);
+
+//console.log(result);
