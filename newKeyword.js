ffb0c66d8a4e746baa4d1f2cfc7d914fcace6a86
Taufiqul Islam <48012661+Taufiqul7756@users.noreply.github.com>
2021-11-18 18:01:11 +0600

    New keyword use

---
+++ newKeyword.js
@@ -0,0 +1,13 @@
+class person {
+  constructor(firstName, lastName, salary) {
+    this.firstName = firstName;
+    this.lastName = lastName;
+    this.salary = salary;
+  }
+}
+const heroPerson = new person("Hero", "Balam", 25000);
+console.log(heroPerson);
+const friendlyPerson = new person("Hero", "Kalam", 25000);
+console.log(friendlyPerson);
+
+console.log(oldPerson);
