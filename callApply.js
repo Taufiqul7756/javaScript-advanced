692c331643a7ecd1140a1919833b6864c9ec910a
Taufiqul Islam <48012661+Taufiqul7756@users.noreply.github.com>
2021-11-18 17:03:40 +0600

    Call,, bine and apply

--- callApply.js
+++ callApply.js
@@ -2,17 +2,21 @@ const normalPerson = {
   firstName: "Rahim",
   lastName: "Uddin",
   salary: 10000,
+
   getFullName: function () {
     console.log(this.firstName, this.lastName);
   },
-  chargeBill: function (amount) {
-    this.salary = this.salary - amount;
+
+  chargeBill: function (amount, tips, tax) {
+    this.salary = this.salary - amount - tips - tax;
     return this.salary;
   },
 };
+
+console.log("Rahim's salary:", normalPerson.salary);
 normalPerson.chargeBill(100);
 normalPerson.chargeBill(400);
-console.log("Rahim(10000):", normalPerson.salary);
+//console.log("Rahim(10000):", normalPerson.salary);
 
 const heroPerson = {
   firstName: "Hero",
@@ -26,11 +30,18 @@ const friendlyPerson = {
   salary: 20000,
 };
 
-const heroChargeBill = normalPerson.chargeBill.bind(heroPerson);
+const heroChargeBill = normalPerson.chargeBill.bind(heroPerson, 990, 10); //call and bind er coma(,) use korbo
 heroChargeBill(2000);
 heroChargeBill(3000);
 console.log("Hero Person(25000):", heroPerson.salary);
 
-const friendlyPersonChargeBill = normalPerson.chargeBill.bind(friendlyPerson);
+const friendlyPersonChargeBill = normalPerson.chargeBill.bind(
+  friendlyPerson,
+  990,
+  10
+);
 friendlyPersonChargeBill(5000);
 console.log("Friendly Person (20000):", friendlyPerson.salary);
+
+normalPerson.chargeBill.apply(heroPerson, [3000, 300, 200]); //apply er jnno array use korbo
+console.log(heroPerson);
