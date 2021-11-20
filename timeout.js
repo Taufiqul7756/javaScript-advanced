fafa9919a43664ffca55dfc5114505709a2b4487
Taufiqul Islam <48012661+Taufiqul7756@users.noreply.github.com>
2021-11-20 15:33:39 +0600

    SetTimeout and setInterval

---
+++ timeout.js
@@ -0,0 +1,13 @@
+function doSomething() {
+  console.log(3333);
+}
+console.log(2222);
+// setTimeout(doSomething, 1000); //ms
+// setTimeout(function () {
+//   console.log("lazyy");
+// }, 2000);
+
+setInterval(() => {
+  console.log("Iam lazzyyy");
+}, 2000);
+console.log(5555);
